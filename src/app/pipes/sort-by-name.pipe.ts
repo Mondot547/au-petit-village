0eb1c46146cb5b1c34dc2da492a573b3c0a8ea42
Mondot547 <alex.mondot.mail.pro@gmail.com>
2024-07-10 01:00:24 +0200

    ajout du message Produit non trouvé lors dune mauvaise recherhe afin dindique aux utilisateur la direction

--- src/app/pipes/sort-by-name.pipe.ts
+++ src/app/pipes/sort-by-name.pipe.ts
@@ -7,12 +7,20 @@ export class SortByNamePipe implements PipeTransform {
 
   transform(products: any[], searchText: string): any[] {
     if (!products || !searchText) {
-      return products;
+      return products; // Retourne tous les produits si la liste est vide ou si le texte de recherche est vide
     }
 
-    return products.filter(product =>
+    // Filtrer les produits dont le nom inclut le texte de recherche (en ignorant la casse)
+    const filteredProducts = products.filter(product =>
       product.name.toLowerCase().includes(searchText.toLowerCase())
     );
+
+    // Si aucun produit ne correspond au critère de recherche, retourner un tableau avec un objet indiquant que le produit n'a pas été trouvé
+    if (filteredProducts.length === 0) {
+      return [{ name: 'Produit non trouvé !'}];
+    }
+
+    return filteredProducts;
   }
 
 }
